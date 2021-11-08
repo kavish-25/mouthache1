d433f149ff7629d2a0d48cc655e39acce3d8cef4
kavish-25 <84278582+kavish-25@users.noreply.github.com>
2021-11-08 19:51:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+noseX=0;
+noseY=0;
+function preload() {
+mouthache=loadImage('https://i.postimg.cc/3x3QzSGq/m.png')
+}
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+    console.log("hi its working")
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+
+}
+function modelLoaded() {
+    console.log('posenet is initialized')
+}
+function gotPoses(results) {
+    if (results.length > 0) {        
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("nose x =" + results[0].pose.nose.x);
+        console.log("nose y =" + results[0].pose.nose.y);
+
+    }
+}
+function draw() {
+    image(video, 0, 0, 300, 300)
+    image(mouthache,noseX,noseY,80,35)
+}
+function take_snapshot() {
+    save('kavish.png');
+}
+
